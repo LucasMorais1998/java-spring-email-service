7281ab1b554bd060c2143ae88705e3613b6f8ab8
Lucas Morais <lucas14.morais@gmail.com>
2023-12-07 18:11:14 -0300

    chore: initialize project

---
+++ src/main/java/com/youtube/javaspringemailservice/JavaSpringEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.javaspringemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringEmailServiceApplication.class, args);
+	}
+
+}
