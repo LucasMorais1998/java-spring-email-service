279c6cdeb789c511858d34690426aabb546eab87
Lucas Morais <lucas14.morais@gmail.com>
2023-12-08 12:36:43 -0300

    refactor(application): improve EmailSenderService method parameter names

--- src/main/java/com/youtube/javaspringemailservice/application/EmailSenderService.java
+++ src/main/java/com/youtube/javaspringemailservice/application/EmailSenderService.java
@@ -1,7 +1,7 @@
 package com.youtube.javaspringemailservice.application;
 
 import com.youtube.javaspringemailservice.adapters.EmailSenderGateway;
-import com.youtube.javaspringemailservice.core.EmailSenderUseCase;
+import com.youtube.javaspringemailservice.core.usecases.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +10,12 @@ public class EmailSenderService implements EmailSenderUseCase {
     private final EmailSenderGateway emailSenderGateway;
 
     @Autowired
-    public EmailSenderService(EmailSenderGateway emailGateway) {
-        this.emailSenderGateway = emailGateway;
+    public EmailSenderService(EmailSenderGateway emailSenderGateway) {
+        this.emailSenderGateway = emailSenderGateway;
     }
 
     @Override
-    public void sendEmail(String to, String subject, String body) {
-        this.emailSenderGateway.sendEmail(to, subject, body);
+    public void sendEmail(String toEmail, String subject, String body) {
+        this.emailSenderGateway.sendEmail(toEmail, subject, body);
     }
 }
