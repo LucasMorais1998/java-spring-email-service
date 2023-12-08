bd531f3a1f8b4fc622e1f73d2c42bc54178ccdb9
Lucas Morais <lucas14.morais@gmail.com>
2023-12-08 12:30:08 -0300

    refactor(infra-ses): improve SesEmailSender variable names and method parameters

--- src/main/java/com/youtube/javaspringemailservice/infra/ses/SesEmailSender.java
+++ src/main/java/com/youtube/javaspringemailservice/infra/ses/SesEmailSender.java
@@ -10,26 +10,26 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SesEmailSender implements EmailSenderGateway {
-    private final AmazonSimpleEmailService amazonSimpleEmailService;
+    private final AmazonSimpleEmailService sesClient;
 
     @Autowired
-    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
-        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    public SesEmailSender(AmazonSimpleEmailService sesClient) {
+        this.sesClient = sesClient;
     }
 
     @Override
-    public void sendEmail(String to, String subject, String body) {
+    public void sendEmail(String toEmail, String subject, String body) {
         SendEmailRequest request = new SendEmailRequest()
                 .withSource("lucas14.morais@gmail.com")
-                .withDestination(new Destination().withToAddresses(to))
+                .withDestination(new Destination().withToAddresses(toEmail))
                 .withMessage(new Message()
                         .withSubject(new Content(subject))
                         .withBody(new Body().withText(new Content(body)))
                 );
         try {
-            this.amazonSimpleEmailService.sendEmail(request);
-        } catch (AmazonServiceException exception) {
-            throw new EmailServiceException("Failure while sending email", exception);
+            this.sesClient.sendEmail(request);
+        } catch (AmazonServiceException ex) {
+            throw new EmailServiceException("Email sending failed.", ex);
         }
     }
 }
