110bd7f5dcbfbe09c11d751722545b72841a93d1
Lucas Morais <lucas14.morais@gmail.com>
2023-12-08 12:38:15 -0300

    refactor(adapter): update EmailSenderGateway method parameter name

--- src/main/java/com/youtube/javaspringemailservice/adapters/EmailSenderGateway.java
+++ src/main/java/com/youtube/javaspringemailservice/adapters/EmailSenderGateway.java
@@ -1,5 +1,5 @@
 package com.youtube.javaspringemailservice.adapters;
 
 public interface EmailSenderGateway {
-    void sendEmail(String to, String subject, String body);
+    void sendEmail(String toEmail, String subject, String body);
 }
